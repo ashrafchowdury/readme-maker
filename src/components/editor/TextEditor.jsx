89c953aa1eae0f0e8d56149763bb7bc9305538a3
Ashraf <ashrafdev365@gmail.com>
2023-02-28 19:49:10 -1200

    fixed the inline selector opening issue

--- src/components/editor/TextEditor.jsx
+++ src/components/editor/TextEditor.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useRef } from "react";
-import ReactQuill, { Quill } from "react-quill";
-// import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
+import { useState, useRef } from "react";
+import ReactQuill from "react-quill";
+import { GrammarlyEditorPlugin } from "@grammarly/editor-sdk-react";
 import "react-quill/dist/quill.snow.css";
 import Selector from "./Selector";
 import InlineSelector from "./InlineSelector";
@@ -89,18 +89,18 @@ const TextEditor = () => {
   };
 
   // handle inline selection component
-  const handleToolTip = (range, source, editor) => {
-    settextSelected(window.getSelection().toString());
-
+  const handleToolTip = () => {
     const quillEdior = editor.current?.getEditor();
     const position = quillEdior?.getBounds(quillEdior.getSelection()?.index);
 
     if (window.getSelection()?.anchorNode.data == undefined) {
       setplaceholder({ ...placeholder, display: true });
+      settextSelected("");
     } else {
       setplaceholder({ ...placeholder, display: false });
+      settextSelected(window.getSelection().toString());
     }
-    if (textSelected != " " || textSelected != "") {
+    if (textSelected != "") {
       setmousePositionForSelectors({
         x: position.left - 80,
         y: position.top - 60,
@@ -108,7 +108,7 @@ const TextEditor = () => {
     }
   };
 
-  const handlePlaceholder = (position, y, size, text) => {
+  const placeholderPosition = (position, y, size, text) => {
     return setplaceholder({
       ...placeholder,
       x: position.left + 5,
@@ -118,19 +118,17 @@ const TextEditor = () => {
     });
   };
 
-  const handleKey = (e) => {
+  const handleSelectorPosition = (e) => {
     e.key == "Enter" && setisSelector(false);
     e.key == "Enter" && setIsKeyEnabled(false);
     const quillEdior = editor.current.getEditor();
     const position = quillEdior.getBounds(quillEdior.getSelection()?.index);
-
+    //
     const windowHeight = document.documentElement.clientHeight;
-
     // Get the position and height of the div
     const divRect = document
       .querySelector(".placeholder")
       ?.getBoundingClientRect();
-
     const bottomSpace = windowHeight - divRect?.top - divRect?.height; // Calculate the space available from the bottom of the div
 
     // this condition for opne & close the selectors
@@ -144,25 +142,32 @@ const TextEditor = () => {
       setisSelector(false);
     }
 
-    handlePlaceholder(position, 0);
+    placeholderPosition(position, 0);
     //this consdition is for align the placeholder
     const tagName = window.getSelection()?.baseNode?.localName;
-    if (tagName == "h1") {
-      handlePlaceholder(position, 10, 30, "Heading 1");
-    } else if (tagName == "h2") {
-      handlePlaceholder(position, 6, 24, "Heading 2");
-    } else if (
-      window.getSelection()?.baseNode?.firstChild?.localName == "img"
-    ) {
+    switch (tagName) {
+      case "h1":
+        placeholderPosition(position, 10, 30, "Heading 1");
+        break;
+      case "h2":
+        placeholderPosition(position, 6, 24, "Heading 2");
+        break;
+      case "p" || "div":
+        placeholderPosition(position, 0, 18, "Press / for open the selectors");
+        break;
+      default:
+        setplaceholder({ ...placeholder, display: false });
+        break;
+    }
+    if (window.getSelection()?.baseNode?.firstChild?.localName == "img") {
       setplaceholder({ ...placeholder, display: false });
-    } else if (tagName == "p" || tagName == "div") {
-      handlePlaceholder(position, 0, 18, "Press / for open the selectors");
-    } else {
-      setplaceholder({ ...placeholder, display: false });
+    }
+    if (window.getSelection()?.anchorNode.data?.length > 10) {
+      saveToLocalStorage(e);
     }
   };
 
-  const handleSaveLocal = (e) => {
+  const saveToLocalStorage = (e) => {
     if (window.getSelection()?.anchorNode?.data?.length > 10) {
       setTimeout(() => {
         setDataSaveIcon(true);
@@ -172,7 +177,7 @@ const TextEditor = () => {
     }
   };
 
-  const handleKeyDown = (event) => {
+  const handleOpenSelector = (event) => {
     if (event.key == "/" && !isKeyEnabled) {
       event.preventDefault();
     }
@@ -195,25 +200,24 @@ const TextEditor = () => {
           placeholder={placeholder}
           setplaceholder={setplaceholder}
         />
-        {/* <GrammarlyEditorPlugin clientId="client_CWguQMGT3AtvUQw7vdoKNk"> */}
-        <div onClick={handleKey}>
-          <ReactQuill
-            ref={editor}
-            theme="snow"
-            value={value}
-            onChange={handleEditorChange}
-            modules={modules}
-            className="editor"
-            onChangeSelection={handleToolTip}
-            onKeyUp={handleKey}
-            onKeyPress={handleSaveLocal}
-            onKeyDown={handleKeyDown}
-          />
-        </div>
-        {/* </GrammarlyEditorPlugin> */}
+        <GrammarlyEditorPlugin clientId="client_CWguQMGT3AtvUQw7vdoKNk">
+          <div onClick={handleSelectorPosition}>
+            <ReactQuill
+              ref={editor}
+              theme="snow"
+              value={value}
+              onChange={handleEditorChange}
+              modules={modules}
+              className="editor"
+              onChangeSelection={handleToolTip}
+              onKeyUp={handleSelectorPosition}
+              onKeyDown={handleOpenSelector}
+            />
+          </div>
+        </GrammarlyEditorPlugin>
         {placeholder.display && (
           <div
-            className={`placeholder absolute z-10 text-lg select-none`}
+            className={`placeholder absolute z-10 text-lg select-none text-slate-600 dark:text-slate-500`}
             style={{
               top: placeholder.y,
               left: placeholder.x,
